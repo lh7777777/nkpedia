a4d0f55a78be2c560e3621e571dc954d36fe722a
Mr-Xindi <1014185005@qq.com>
2019-07-14 19:02:39 +0800

    xd 0714 index页面美化 去掉信封 修改四个Controller 把yyz加入开发者 下拉菜单

--- backend/views/pedia-user-adorn/_form.php
+++ backend/views/pedia-user-adorn/_form.php
@@ -3,6 +3,11 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+/**
+ * Team:没有蛀牙 NKU
+ * Coding by 王心荻 1711298 20190714
+ */
+
 /* @var $this yii\web\View */
 /* @var $model common\models\PediaUserAdorn */
 /* @var $form yii\widgets\ActiveForm */
@@ -14,7 +19,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'uid')->textInput() ?>
 
-    <?= $form->field($model, 'mid')->textInput() ?>
+    <?=
+    $form->field($model,'mid')->dropDownList(Yii::$app->db->createCommand('SELECT mid FROM pedia_user_medal')->queryColumn());
+    //$form->field($model, 'mid')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
