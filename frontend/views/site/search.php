5409aae1257176cd9774c0895e74120184842163
gilsaia <794433219@qq.com>
2019-07-15 11:25:59 +0800

    frontendpage

--- frontend/views/site/search.php
+++ frontend/views/site/search.php
@@ -45,9 +45,41 @@ $this->title = 'My Yii Application';
             </div>
         </div>
         <div class="layui-colla-item">
-            <h2 class="layui-colla-title layui-bg-gray">简介</h2>
+            <h2 class="layui-colla-title layui-bg-gray">基本内容</h2>
             <div class="layui-colla-content layui-show">
-                <?=Html::encode("$word->brief_info")?>
+                <ul class="layui-timeline">
+                    <li class="layui-timeline-item">
+                        <i class="layui-icon layui-timeline-axis layui-bg-gray">&#xe63f;</i>
+                        <div class="layui-timeline-content layui-text">
+                            <h3 class="layui-timeline-title">词条简介</h3>
+                            <?=Html::encode("$word->brief_info")?>
+                        </div>
+                    </li>
+                    <li class="layui-timeline-item">
+                        <i class="layui-icon layui-timeline-axis layui-bg-gray">&#xe63f;</i>
+                        <div class="layui-timeline-content layui-text">
+                            <h3 class="layui-timeline-title">词条类别</h3>
+                            <p>杜甫的思想核心是儒家的仁政思想，他有“<em>致君尧舜上，再使风俗淳</em>”的宏伟抱负。个人最爱的名篇有：</p>
+                            <ul>
+                                <li>《登高》</li>
+                                <li>《茅屋为秋风所破歌》</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="layui-timeline-item">
+                        <i class="layui-icon layui-timeline-axis layui-bg-gray">&#xe63f;</i>
+                        <div class="layui-timeline-content layui-text">
+                            <h3 class="layui-timeline-title">相关词汇</h3>
+                            <p>
+                                中国人民抗日战争胜利72周年
+                                <br>常常在想，尽管对这个国家有这样那样的抱怨，但我们的确生在了最好的时代
+                                <br>铭记、感恩
+                                <br>所有为中华民族浴血奋战的英雄将士
+                                <br>永垂不朽
+                            </p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="layui-colla-item">
