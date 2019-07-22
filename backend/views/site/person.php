f3c7535b1a2c96dc1ee6a5c8bd74d0eb044e1c16
lh7777777 <1623773848@qq.com>
2019-07-22 12:38:42 +0800

    个人作业

--- backend/views/site/person.php
+++ backend/views/site/person.php
@@ -16,31 +16,43 @@ $this->title = 'PersonWork';
     <div class="clearfix"></div>
     <div id="w0" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 杨俣哲</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
         <p style="width: 100%;height: 20px;display: block;line-height: 30px;text-align: left;font-size: 20px">
-            <a href="/nkpedia/data/personal/作业1(1711396_杨俣哲).pdf" class="btn btn-primary">作业一</a>
+            <a href="/nkpedia/data/personal/作业1(1711396_杨俣哲).pdf" class="btn btn-primary">作业一文档</a>
             <a href="/nkpedia/data/personal/作业2(1711396_杨俣哲).pdf" class="btn btn-success">作业二文档</a>
             <a href="/nkpedia/data/personal/作业2(1711396_杨俣哲).rp" class="btn btn-warning">作业二文件</a>
             <a href="/nkpedia/data/personal/作业2(1711396_杨俣哲).zip" class="btn btn-danger">作业二压缩包</a>
-            <a href="/nkpedia/data/personal/作业3(1711396_杨俣哲).pdf" class="btn btn-info">作业三</a>
+            <a href="/nkpedia/data/personal/作业3(1711396_杨俣哲).pdf" class="btn btn-info">作业三文档</a>
         </p>
         <!--<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only">70% Complete</span></div>-->
     </div>
     <div id="w1" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 孙一冉</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
         <p style="width: 100%;height: 20px;display: block;line-height: 30px;text-align: left;font-size: 20px">
+            <a href="/nkpedia/data/personal/作业1(1711297_孙一冉).pdf" class="btn btn-primary">作业一文档</a>
+            <a href="/nkpedia/data/personal/作业2(1711297_孙一冉).zip" class="btn btn-success">作业二压缩包</a>
+            <a href="/nkpedia/data/personal/作业3(1711297_孙一冉).pdf" class="btn btn-warning">作业三文档</a>
         </p>
         <!--<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only">70% Complete</span></div>-->
     </div>
-    <div id="w2" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 杨越</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
+    <div id="w2" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 王心荻</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
         <p style="width: 100%;height: 20px;display: block;line-height: 30px;text-align: left;font-size: 20px">
+            <a href="/nkpedia/data/personal/作业1(1711298_王心荻).pdf" class="btn btn-primary">作业一文档</a>
+            <a href="/nkpedia/data/personal/作业2(1711298_王心荻).zip" class="btn btn-success">作业二压缩包</a>
+            <a href="/nkpedia/data/personal/作业3(1711298_王心荻).pdf" class="btn btn-warning">作业三文档</a>
         </p>
         <!--<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only">70% Complete</span></div>-->
     </div>
-    <div id="w3" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 王心荻</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
+    <div id="w3" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 杨越</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
         <p style="width: 100%;height: 20px;display: block;line-height: 30px;text-align: left;font-size: 20px">
+            <a href="/nkpedia/data/personal/作业1(1711300_杨越).pdf" class="btn btn-primary">作业一文档</a>
+            <a href="/nkpedia/data/personal/作业2(1711300_杨越).zip" class="btn btn-success">作业二压缩包</a>
+            <a href="/nkpedia/data/personal/作业3(1711300_杨越).pdf" class="btn btn-warning">作业三文档</a>
         </p>
         <!--<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only">70% Complete</span></div>-->
     </div>
     <div id="w4" class="x_panel"><div class="x_title"><h1><i class="fa fa-tasks"></i> 解亚兰</h1><div class="clearfix"></div></div><!--<div class="x_content"><div id="w1" class="active progress-striped progress">-->
         <p style="width: 100%;height: 20px;display: block;line-height: 30px;text-align: left;font-size: 20px">
+            <a href="/nkpedia/data/personal/作业1(1711431_解亚兰).pdf" class="btn btn-primary">作业一文档</a>
+            <a href="/nkpedia/data/personal/作业2(1711431_解亚兰).zip" class="btn btn-success">作业二压缩包</a>
+            <a href="/nkpedia/data/personal/作业3(1711431_解亚兰).pdf" class="btn btn-warning">作业三文档</a>
         </p>
         <!--<div class="progress-bar-success progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%"><span class="sr-only">70% Complete</span></div>-->
     </div>
